b30f3965e609519f891f74bc239ecbeb3d4606f1
bernard rayoso <rayosobernard@gmail.com>
2023-08-09 01:34:27 +0800

    fix hover issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {},
   plugins: [],
+  variants: {
+    extend: {
+      backgroundColor: ["active"],
+      // ...
+      borderColor: ["focus-visible", "first"],
+      // ...
+      textColor: ["visited", "hover"],
+    },
+  },
 };
