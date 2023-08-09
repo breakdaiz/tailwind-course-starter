69efb9cc04d3ec2c89b2958284515b167bbeefc1
bernard rayoso <rayosobernard@gmail.com>
2023-08-10 01:58:58 +0800

    Mobile navbar animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {},
-  plugins: [],
+  plugins: [require("./plugins/openVariant")],
   variants: {
     extend: {
       backgroundColor: ["active"],
