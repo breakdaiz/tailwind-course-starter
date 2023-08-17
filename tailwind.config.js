c4b5b99cf714d8b7065aed94b349f0e226cffb8d
bernard rayoso <rayosobernard@gmail.com>
2023-08-18 02:10:08 +0800

    add table element, create plugin table caption-bottom

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,8 @@ module.exports = {
   },
   plugins: [
     require("./plugins/openVariant"), 
-    require("./plugins/animationDelay")
+    require("./plugins/animationDelay"),
+    require("./plugins/tableCaption"),
   ],
 
 };
